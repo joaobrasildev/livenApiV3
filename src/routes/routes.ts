f79c267c91f69387899a228a8c5b405a3c54ae68
Joao Brasil <jvobsystems@gmail.com>
2022-05-02 08:56:33 -0300

    Adding new routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,11 @@ import * as userGetAll from '../modules/users/getAll/controller'
 import * as userGetOne from '../modules/users/getOne/controller'
 import * as userUpdate from '../modules/users/update/controller'
 import * as userDelete from '../modules/users/delete/controller'
+import * as addressCreate from '../modules/address/create/controller'
+import * as addressGetAll from '../modules/address/getAll/controller'
+import * as addressGetOne from '../modules/address/getOne/controller'
+import * as addressUpdate from '../modules/address/update/controller'
+import * as addressDelete from '../modules/address/delete/controller'
 import { validate } from '../shared/middleware/validate'
 import { createUserSchema, updateUserSchema } from '../shared/validators/user'
 
@@ -14,5 +19,11 @@ router.get("/users", userGetAll.getAll);
 router.get("/users/:id", userGetOne.getOne);
 router.put("/users", validate(updateUserSchema), userUpdate.update);
 router.delete("/users/:id", userDelete.remove);
+
+router.post("/addresses", addressCreate.create);
+router.get("/addresses", addressGetAll.getAll);
+router.get("/addresses/:id", addressGetOne.getOne);
+router.put("/addresses", addressUpdate.update);
+router.delete("/addresses/:id", addressDelete.remove);
 
 export default router;
