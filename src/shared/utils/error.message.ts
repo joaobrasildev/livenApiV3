5e87676ad85aa7850745375829735df8ed3f3535
Joao Brasil <jvobsystems@gmail.com>
2022-05-02 08:50:07 -0300

    Adding erroe message and status code

--- src/shared/utils/error.message.ts
+++ src/shared/utils/error.message.ts
@@ -2,4 +2,5 @@ export enum Error {
     UserNotFound = 'User not found',
     UserEmailAlreadyExist = 'User email already exist',
     UserCpfAlreadyExist = 'User cpf already exist',
+    AddressNotFound = 'Address not found',
 }
--- src/shared/utils/error.statusCode.ts
+++ src/shared/utils/error.statusCode.ts
@@ -2,5 +2,6 @@ export enum StatusCode {
   NotFound = 404,
   ConflictException = 409,
   Created = 201,
-  Ok = 200
+  Ok = 200,
+  NoContent = 204
 }
