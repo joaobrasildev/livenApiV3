bf7545da3540a0509874951c54cf571e459f6e24
Joao Brasil <jvobsystems@gmail.com>
2022-05-02 01:15:14 -0300

    Adding validators in routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,13 +4,15 @@ import * as userGetAll from '../modules/users/getAll/controller'
 import * as userGetOne from '../modules/users/getOne/controller'
 import * as userUpdate from '../modules/users/update/controller'
 import * as userDelete from '../modules/users/delete/controller'
+import { validate } from '../shared/middleware/validate'
+import { createUserSchema, updateUserSchema } from '../shared/validators/user'
 
 const router = Router();
 
-router.post("/users", userCreate.create);
+router.post("/users", validate(createUserSchema), userCreate.create);
 router.get("/users", userGetAll.getAll);
 router.get("/users/:id", userGetOne.getOne);
-router.put("/users", userUpdate.update);
+router.put("/users", validate(updateUserSchema), userUpdate.update);
 router.delete("/users/:id", userDelete.remove);
 
 export default router;
---
+++ src/shared/validators/user.ts
@@ -0,0 +1,80 @@
+import * as yup from "yup";
+import { ValidatorMessages } from "../utils/validator.message";
+import { REGEX_VALIDATOR_DATE, REGEX_VALIDATOR_UUID } from "../utils/validator.regex";
+
+export const createUserSchema = yup.object().shape({
+  body: yup.object({
+    name: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("name"))
+      .max(200, ValidatorMessages.maxLengthMessage("name", "200")),
+    email: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("email"))
+      .max(200, ValidatorMessages.maxLengthMessage("email", "200")), 
+    password: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("password"))
+      .max(200, ValidatorMessages.maxLengthMessage("password", "200")),            
+    cpf: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("cpf"))
+      .max(200, ValidatorMessages.maxLengthMessage("cpf", "11")),            
+    birthDate: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("birthDate"))
+      .matches(REGEX_VALIDATOR_DATE)
+      .max(50, ValidatorMessages.maxLengthMessage("birthDate", "50")),
+    phone: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("phone"))
+      .max(100, ValidatorMessages.maxLengthMessage("phone", "14")),
+  }),
+});
+
+export const updateUserSchema = yup.object().shape({
+  body: yup.object({
+    id: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("id"))
+      .matches(REGEX_VALIDATOR_UUID, {message: ValidatorMessages.mustBeMessage("id", "uuid")}),
+    name: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("name"))
+      .max(200, ValidatorMessages.maxLengthMessage("name", "200")),
+    email: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("email"))
+      .max(200, ValidatorMessages.maxLengthMessage("email", "200")), 
+    password: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("password"))
+      .max(200, ValidatorMessages.maxLengthMessage("password", "200")),            
+    cpf: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("cpf"))
+      .max(200, ValidatorMessages.maxLengthMessage("cpf", "11")),            
+    birthDate: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("birthDate"))
+      .matches(REGEX_VALIDATOR_DATE)
+      .max(50, ValidatorMessages.maxLengthMessage("birthDate", "50")),
+    phone: yup
+      .string()
+      .strict(true)
+      .required(ValidatorMessages.requiredMessage("phone"))
+      .max(100, ValidatorMessages.maxLengthMessage("phone", "14")),
+  }),
+});
