8976e950ff2317edd6406a33291994aa1158e731
Joao Brasil <jvobsystems@gmail.com>
2022-05-02 08:49:19 -0300

    Adding Address dtos

---
+++ src/shared/dtos/address/update.dto.ts
@@ -0,0 +1,12 @@
+export class UpdateAddressDto {
+  id: string;
+  postalCode: string;
+  street: string;
+  number: number;
+  complement: string;
+  neighborhood: string;
+  state: string;
+  city: string;
+  country: string;
+  userId: string;
+}
